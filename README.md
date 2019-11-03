1055370ba34a42d5c876004a61f8a5252f6787ec
mits58 <ynakano927@gmail.com>
2019-11-03 15:58:42 +0900

    implemented reading data

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Pointer-Networks
 An implementation of Pointer Networks in chainer
+
+# Todo
+- ~~ データを扱うためのクラス定義と、実データを読み込むまでをやる（やるだけ）~~
+  - ~~ データは ((x1, x2, ..., xn), (t1, t2, t3……))　みたいな感じ　もちろんnは可変 ~~
+- Pointer Network そのものの実装（たいへん）
+  - Seq2Seq部分の実装
+    - Encoder部分の実装
+    - Decoder部分の実装
+    - 組合せたネットワークの実装？
+  - Attention部分の実装
+    - よくわからん
+  - PointerNetworkの実装
+    - わからん
+- 実際に回してみる（やるだけ）
+
+# Links
+- Pointer Networksのデータセット
+  - https://drive.google.com/drive/folders/0B2fg8yPGn2TCMzBtS0o4Q2RJaEU
---
+++ util.py
@@ -0,0 +1,62 @@
+import collections
+
+import chainer
+import numpy as np
+
+np.random.seed(1124) # noa himesaka is so cute :)
+
+class PlaneData(chainer.dataset.DatasetMixin):
+    """
+    A class for point data.
+    Attributes
+    ----------
+    must implement len, get_example, converter
+    """
+
+    def __init__(self, dataset, device):
+        # points -> x, perms -> t
+        self.points, self.perms = PlaneData.load_data(dataset, device)
+
+    def __len__(self):
+        return len(self.points)
+
+    def get_example(self, i):
+        return self.points[i], self.perms[i]
+
+    def converter(self, datalist, device):
+        xp = chainer.get_device(device).xp
+        batch_points = [data[0] for data in datalist]
+        batch_perms = [data[0] for data in datalist]
+        return batch_poins, batch_perms
+
+    def load_data(dataset, device):
+        """
+        Only for 10 points TSP...
+        """
+
+        if device == -1:
+            xp = np
+        else:
+            xp = device.xp
+
+
+        with open('./data/tsp_10_train_exact.txt', 'r') as f:
+            data = f.readlines()
+
+        points = []
+        perms = []
+
+        for d in data:
+            pt, pm = [tmp.strip() for tmp in d.split("output")]
+            pt = xp.array([float(val) for val in pt.split(" ")], dtype=np.float32)
+            pm = xp.array([int(num) for num in pm.split(" ")])
+
+            points.append(pt)
+            perms.append(pm)
+
+        return points, perms
+
+
+
+if __name__ == '__main__':
+    PlaneData.load_data("hoge", -1)
